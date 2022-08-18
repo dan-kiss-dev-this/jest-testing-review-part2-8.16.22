6fcc04c1592177679091ed138aeca6845a9d2c02
Dan Kiss <flowfast47@gmail.com>
2022-08-17 17:01:15 -0700

    add a wrapper for testing and adjust components

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -1,11 +1,44 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import App from '../containers/App.js';
+import MainPage from './MainPage';
 
-it('should render the app component', () => {
-  const mockStore = {
+let wrapper;
+// beforeEach comes with jest
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
     robots: [],
     searchField: '',
+    isPending: false,
   };
-  expect(shallow(<App store={mockStore} />)).toMatchSnapshot();
+  wrapper = shallow(<MainPage {...mockProps} />);
+});
+
+it('should render the MainPage component without crashing', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('filters robots correctly', () => {
+  const mockProps2 = {
+    onRequestRobots: jest.fn(),
+    robots: [
+      {
+        id: 3,
+        name: 'John',
+        email: 'john@gmail.com',
+      },
+    ],
+    searchField: 'John',
+    isPending: false,
+  };
+  // instance gets us access to methods
+  expect(wrapper.instance().filterRobots([])).toEqual([]);
+  wrapper = shallow(<MainPage {...mockProps2} />);
+  expect(wrapper.instance().filterRobots([])).toEqual([
+    {
+      id: 3,
+      name: 'John',
+      email: 'john@gmail.com',
+    },
+  ]);
 });
